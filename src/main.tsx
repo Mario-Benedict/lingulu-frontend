9c66d6da171434f5ff8ecc119eb4ff30ab955f8a
Mario-Benedict <mariobenedict77@gmail.com>
2025-11-28 09:06:52 +0700

    refactor: move assets from public to src and add alias configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
-import Login from './pages/auth/Login.tsx'
-import Register from './pages/auth/Register.tsx'
-import NotFoundPage from './pages/NotFoundPage.tsx'
+import Login  from '@pages/auth/Login.tsx'
+import Register from '@pages/auth/Register.tsx'
+import NotFoundPage from '@pages/NotFoundPage.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
