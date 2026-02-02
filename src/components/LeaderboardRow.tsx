956db683fec0a9f78c8eb18f6745a8a380c426c1
Mario-Benedict <mariobenedict77@gmail.com>
2026-02-02 10:36:49 +0700

    fix: merge conflict

--- src/components/LeaderboardRow.tsx
+++ src/components/LeaderboardRow.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from 'react';
+import React from 'react';
 
 interface LeaderboardRowProps {
   rank: number;
--- src/components/LessonCircleButton.tsx
+++ src/components/LessonCircleButton.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from 'react';
+import React from 'react';
 import { Lock, CheckCircle } from 'lucide-react';
 import type { LessonCircleButtonProps } from '@types/index';
 
